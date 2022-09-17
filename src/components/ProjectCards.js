d38d38bb56045855998b30e1a74d72e3c329e67a
Anne McCorkle <annefmccorkle@gmail.com>
2022-09-17 09:35:20 -0600

    images on projects page with no error now, other errors still

---
+++ src/components/ProjectCards.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Projects from '../components/pages/Projects';
+import '../App.css';
+
+function ProjectCards(props) {
+   return (
+     <div className={`grid-item gallery ${props.id}`}>
+       <img src={props.image} alt={props.title} className="gallery-img" />
+       <div className="description">
+         <h4>{props.title}</h4>
+       </div>
+
+       <div className="links">
+         <a href={props.github} target="_blank" rel="noreferrer" alt="Github Repository">Github Repository</a>
+         <a href={props.deployed} target="_blank" rel="noreferrer" alt="Deployed app">Deployed App</a>
+       </div>
+   </div>
+   );
+ }
+
+export default ProjectCards;
